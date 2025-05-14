27875ceefda3a5fad842157b3e6f4df73152fb75
Daniel Martins de Oliveira <155649847+danielmartinscode@users.noreply.github.com>
2025-05-14 20:44:57 -0300

    Finalizando aula03

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let listaDeItens = []
+let itemAEditar
 
 const form = document.getElementById('form-itens')
 const itensInput = document.getElementById('receber-item')
@@ -8,7 +9,7 @@ const ulItensComprados = document.getElementById('itens-comprados')
 form.addEventListener('submit', function (evento) {
     evento.preventDefault()
     salvarItem()
-    mostrarItens()
+    mostrarItem()
     itensInput.focus()
 })
 
@@ -26,7 +27,7 @@ function salvarItem() {
     itensInput.value = ''
 }
 
-function mostrarItens() {
+function mostrarItem() {
     ulItens.innerHTML = ''
     ulItensComprados.innerHTML = ''
     listaDeItens.forEach((elemento, index) => {
@@ -44,9 +45,11 @@ function mostrarItens() {
             ulItens.innerHTML += `<li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
                <div>
                    <input type="checkbox" class="is-clickable" />
-                   <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+                   <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled' : ''}></input>
                </div>
+
                <div>
+                   ${index === Number(itemAEditar) ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>' : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'}
                    <i class="fa-solid fa-trash is-clickable deletar"></i>
                </div>
            </li>`
@@ -58,7 +61,35 @@ function mostrarItens() {
         i.addEventListener('click', (evento) => {
             const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens[valorDoElemento].checar = evento.target.checked
-            mostrarItens()
+            mostrarItem()
+        })
+    })
+
+    const deletarObjetos = document.querySelectorAll('.deletar')
+    deletarObjetos.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            listaDeItens.splice(valorDoElemento, 1)
+            mostrarItem()
+        })
+    })
+
+    const editarItens = document.querySelectorAll('.editar')
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
         })
     })
 }
+
+function salvarEdicao() {
+    const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
+    // console.log(itemEditado.value)
+    listaDeItens[itemAEditar].valor = itemEditado.value
+    console.log(listaDeItens)
+    itemAEditar = -1
+    mostrarItem()
+}
+
+
